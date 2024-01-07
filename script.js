9a825dc2475ac6f672a96a4d649f53cb27584bf5
LoiChouette2036 <tristanbellanger10@gmail.com>
2024-01-07 19:46:30 +0900

    Finished to write the structure of the function playRound. Need to refine

--- script.js
+++ script.js
@@ -11,3 +11,47 @@ function getComputerChoice (){
     return choicesRandom;
 }
 
+function playRound(playerSelection, computerSelection){
+    // Convert playerSelection to lowercase for case-insensitive comparison
+    let lowerCaseSelection = playerSelection.toLowerCase();
+
+    // initize variable for winner
+    let win ;
+
+    switch (lowerCaseSelection){
+        case "rock":
+            if(computerSelection === "Paper"){
+                return 0;
+            } else if (computerSelection === "Scissor"){
+                return 1;
+            } else if (computerSelection === "Rock"){
+                return 2;
+            } else{
+                console.log("Il y a un problème il faut recommencer");
+                break;
+            }
+        case "paper":
+            if (computerSelection === "Rock"){
+                return 1;
+            } else if (computerSelection === "Scissor"){
+                return 0;
+            } else if (computerSelection === "Paper"){
+                return 2;
+            } else {
+                console.log("Il y a un problème il faut recommencer");
+                break;
+            }
+        case "rock":
+            if(computerSelection === "Paper"){
+                return 0;
+            } else if (computerSelection === "Scissor"){
+                return 1;
+            } else if (computerSelection === "Rock"){
+                return 2;
+            } else {
+                console.log("Il y a un problème, il faut recommencer");
+                break;
+            }
+
+    }
+}
